ed679d27ea10c23cc2a8a8acb811fd8aafe42b66
joelcr10 <joelcraju@gmail.com>
2023-07-25 18:28:56 +0530

    I/O, debugging and datatypes

---
+++ dataypes.java
@@ -0,0 +1,16 @@
+
+
+
+public class dataypes {
+    public static void main(String[] args){
+        int rollno = 65;
+        char letter = 'r';
+        float marks = 878.67f;
+        double largeDecimalNumbers = 1212323.2312313;
+        long largeInteger = 1231241241241244L;
+        boolean check = false;
+        byte a = 40;
+        short s = 1024;
+
+    }
+}
---
+++ inputs.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class inputs {
+    public static void main(String[] args){
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Please enter some number: ");
+        int rollno = inp.nextInt();
+        System.out.println("the rollno is "+rollno);
+
+        System.out.print("Enter name: ");
+        String name = inp.next();
+        System.out.println("the name is "+name);
+
+        System.out.print("Enter float number: ");
+        float fl = inp.nextFloat();
+        System.out.println("the float  is "+fl);
+
+       
+
+    }
+}
